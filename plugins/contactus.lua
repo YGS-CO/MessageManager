85df86490772457f74b90de5cc297001ccd68f32
NavidQuick <navid.kakavand2@gmail.com>
2016-09-05 02:45:32 +0430

    Update contactus.lua

--- plugins/contactus.lua
+++ plugins/contactus.lua
@@ -71,7 +71,8 @@ end
    msg = msg.reply_to_message
     local receiver = msg.forward_from.id
     local input = blocks[1]
-      api.sendMessage(receiver, '`جواب اوپراتور`\n\n'..input, false)
+    local txt = "*جواب اوپراتور*\n\n"..input, false
+      api.sendMessage(receiver, txt)
 end
 end
 return {
