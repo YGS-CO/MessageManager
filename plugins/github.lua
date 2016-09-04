243cd48597316eac8ef8bed98dfe4bcbfb17ca5c
NavidQuick <navid.kakavand2@gmail.com>
2016-09-05 02:13:47 +0430

    Update github.lua

--- plugins/github.lua
+++ plugins/github.lua
@@ -2,9 +2,10 @@ local function run(msg)
   local text = "*Tele Surena Github*"
     local keyboard = {}
     keyboard.inline_keyboard = {
-   {
-               {text = 'admin', url = 'https://github.com/SurenaTeam/PmResan'},
+    {
+               {text = 'Github', url = 'https://github.com/SurenaTeam'},
       },
+    }
  send_api_keyboard(msg, get_receiver_api(msg), text, keyboard)
   end
 return { 
